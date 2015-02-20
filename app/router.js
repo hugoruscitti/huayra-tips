1aa04ee5e248b4f455be13fa0940b3c044b35b6f
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-02-20 16:56:04 -0300

    actualizacion...

--- app/router.js
+++ app/router.js
@@ -6,6 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('index', {path: '/'});
+  this.resource("actualizar", {path: "actualizar/:package"});
 });
 
 export default Router;
